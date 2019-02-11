71914a6e3b558955cf8daf59d952a243bd176957
iraimiks <r.bortascenoks@gmail.com>
2019-02-11 21:13:09 +0200

    Double using

--- src/lv/udemyexmeple1/Main.java
+++ src/lv/udemyexmeple1/Main.java
@@ -3,7 +3,13 @@ package lv.udemyexmeple1;
 public class Main {
 
     public static void main(String[] args){
+        double numbersInPounds;
+        double oneKgInPounds = 0.45359237;
+        double numbersKgToConvert = 20.0;
 
+        numbersInPounds = numbersKgToConvert * oneKgInPounds;
+        System.out.println(numbersInPounds + " Kg");
+        System.out.println(numbersInPounds);
 
     }
 }
